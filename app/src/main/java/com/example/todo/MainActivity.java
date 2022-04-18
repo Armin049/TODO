5d0c9039c9fb904e2a869bdee7e9d6d150e2467a
Weber, Armin <Armin.Weber@adesso.de>
2022-04-18 12:07:42 +0200

    change screen added

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.todo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void NewActivity(View view) {
+        Button button = findViewById(R.id.button);
+        setContentView(R.layout.activity_detail);
+    }
+
+    public void cancel(View view) {
+        Button button = findViewById(R.id.cancel);
         setContentView(R.layout.activity_main);
     }
 }
